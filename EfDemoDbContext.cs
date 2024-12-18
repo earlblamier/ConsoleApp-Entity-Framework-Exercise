28d96dbe1a0f0ad9296ebd7cff8ad6ddf89b2f7d
earlblamier <earlblamier@gmail.com>
2024-12-17 19:40:32 -0500

    Add project files.

---
+++ EfDemoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleAppEfDemo
+{
+    public class EfDemoDbContext : DbContext
+    {
+        private readonly string connstring;
+
+        public DbSet<Product> Products { get; set; }
+        public EfDemoDbContext(string connstring)
+        {
+            this.connstring = connstring;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleAppEfDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectingString = "Server=RYZ7ELPC\\SQLEXPRESS;Database=EfDemo; Trusted_Connection=True";
+            using (var context = new EfDemoDbContext(connectingString))
+            {
+                var androidCollection = context.Products.Where(p => p.Name.StartsWith("Android"));
+                foreach (var product in androidCollection)
+                {
+                    Console.WriteLine($"#{product.Id}:{product.Name}");
+                }
+            }
+        }
+    }
+}
